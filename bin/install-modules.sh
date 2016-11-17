3fe6d5fefd08c67053786d8e250bb541ab6d1e36
Derek Nutile <dereknutile@gmail.com>
2016-11-17 07:31:23 -0800

    add/sh Create install modules script

---
+++ bin/install-modules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# modules directory
+modules_dir=/vagrant/public/modules
+
+echo "Clearing cache ..."
+drush cache-clear all
+
+echo "Install modules ..."
+drush dl yamlform --destination=$modules_dir
+
+echo "Enable modules(s)"
+drush en yamlform
