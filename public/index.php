35dc1a8788cf8d7fce9bac6e338a2dbffa6fa41f
Derek Nutile <dereknutile@gmail.com>
2016-11-21 14:03:20 -0800

    add/feature Install Drupal

--- public/index.php
+++ public/index.php
@@ -1,57 +1,22 @@
-<html>
-  <head>
-    <title>VLEMP</title>
+<?php
 
-    <link href='//fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
-    <link href='//fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
 
-    <style>
-      body {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100%;
-        color: #aaa;
-        display: table;
-        font-weight: 100;
-        font-family: 'Roboto';
-        text-align: center;
-      }
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
 
-      .container {
-        text-align: center;
-        display: table-cell;
-        vertical-align: middle;
-      }
+$autoloader = require_once 'autoload.php';
 
-      .content {
-        width: 640px;
-        display: inline-block;
-      }
+$kernel = new DrupalKernel('prod', $autoloader);
 
-      .title {
-        font-family: 'Raleway';
-        font-size: 48px;
-        color: #001;
-        margin-bottom: 40px;
-      }
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
 
-      p {
-        font-size: 20px;
-        line-height: 30px;
-        color: #011;
-        text-align: left;
-      }
-    </style>
-  </head>
-
-  <body>
-    <div class="container">
-      <div class="content">
-        <div class="title"><?php echo "Your Vagrant LEMP installation is good to go!"; ?></div>
-        <p>This index.php file is located in the root of this installation under <code>/public/</code>.  You can delete this index.php file and start developing your code in the public directory now!</p>
-        <p>The <a href="https://github.com/dereknutile/vlemp" target="_blank">VLEMP Github repository</a> has more information about the default server configuration, installed software, and tools.</p>
-      </div><!-- /.content -->
-    </div><!-- /.container -->
-  </body>
-</html>
+$kernel->terminate($request, $response);
